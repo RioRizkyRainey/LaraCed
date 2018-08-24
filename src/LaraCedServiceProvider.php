7a8a480c193e2c135acf0dd2f5eba717bd740cdd
Rio Rizky Rainey Ferbiansyah <rizkyrainey@gmail.com>
2018-08-24 23:00:41 +0700

    Add custom column name

--- src/LaraCedServiceProvider.php
+++ src/LaraCedServiceProvider.php
@@ -18,21 +18,23 @@ class LaraCedServiceProvider extends ServiceProvider
             $this->unsignedInteger('updated_by')->nullable()->index();
             $this->unsignedInteger('deleted_by')->nullable()->index();
         });
-        Blueprint::macro('creator', function() {
-            $this->unsignedInteger('created_by')->nullable()->index();
-            $this->unsignedInteger('updated_by')->nullable()->index();
-            $this->unsignedInteger('deleted_by')->nullable()->index();
+
+        Blueprint::macro('creator', function($name) {
+            if (!isset($name) or empty($name)) 
+                $name = 'created_by';
+            $this->unsignedInteger($name)->nullable()->index();
         });
-        Blueprint::macro('editor', function() {
-            $this->unsignedInteger('created_by')->nullable()->index();
-            $this->unsignedInteger('updated_by')->nullable()->index();
-            $this->unsignedInteger('deleted_by')->nullable()->index();
+        Blueprint::macro('editor', function($name) {
+            if (!isset($name) or empty($name)) 
+                $name = 'updated_by';
+            $this->unsignedInteger($name)->nullable()->index();
         });
-        Blueprint::macro('destroyer', function() {
-            $this->unsignedInteger('created_by')->nullable()->index();
-            $this->unsignedInteger('updated_by')->nullable()->index();
-            $this->unsignedInteger('deleted_by')->nullable()->index();
+        Blueprint::macro('destroyer', function($name) {
+            if (!isset($name) or empty($name)) 
+                $name = 'deleted_by';
+            $this->unsignedInteger($name)->nullable()->index();
         });
+
         Blueprint::macro('dropCed', function() {
             $this->dropColumn(['created_by', 'updated_by', 'deleted_by']);
         });
